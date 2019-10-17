ea6333bd12de44a8d694387651229555eaff2633
yanzi-PC <chuyan0421@126.com>
2019-10-17 22:57:19 +0800

    before work

---
+++ q148.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+ 
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        
+    }
+};
+
+int main(int argc, char *argv[]){
+
+
+    
+}
